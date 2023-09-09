85d611078d82f4ad23578a17cbe8da11db5b4a57
Joseph Jayeoba <josephbusayojayeoba@gmail.com>
2023-09-09 06:58:23 +0100

    changed utc format

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.get('/api', (req, res) => {
         res.send({
             "slack_name": slack_name,
             "current_day": currentDay,
-            "utc_time": new Date().toISOString(),
+            "utc_time": new Date().toISOString().split('.')[0] + 'Z',
             "track": track,
             "github_file_url": "https://github.com/JosephBusayo/hng_task1/blob/main/index.js",
             "github_repo_url": "https://github.com/JosephBusayo/hng_task1",
@@ -33,6 +33,11 @@ app.get('/api', (req, res) => {
         res.send(err)
     }
 })
+const secondLevelPrecision = date.toISOString().split('.')[0] + 'Z';
+const millisecondLevelPrecision = `${date.toISOString().slice(0, -1)}${date.getMilliseconds()}Z`;
+
+console.log("Second-level precision:", secondLevelPrecision);
+console.log("Millisecond-level precision:", millisecondLevelPrecision);
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)
